ef15ad5ac49765d264ed6dbdc434a7fc8b2b030b
Veronica Lopez <nicaa0695@gmail.com>
2019-07-29 17:49:27 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,16 @@
-class Person
+#class Person
+#  def initialize(name)
+#    @name=name 
+#  end
+#  def name
+#    @name
+#  end
+#  def name=(new_name)
+#    @name=new_name
+#  end
+#end
+class Person 
   def initialize(name)
-    @name=name 
-  end
-  def name
-    @name
-  end
-  def name=(new_name)
-    @name=new_name
+    @name=name
   end
 end
